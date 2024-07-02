cd4e6e7bb7e3e6a2df17fe2599645dae90b4c9cf
Alexander Grenkov <a.grenkov@gmail.com>
2024-07-02 10:44:24 +0500

    add: tagging

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -42,6 +43,8 @@ class Post(models.Model):
             "blog:post_detail",
             args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
         )
+
+    tags = TaggableManager()
 
 
 class Comment(models.Model):
